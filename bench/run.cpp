31d8583b0822120e25267e2c2ad6898b3541cf5d
gretacb <carol@mapbox.com>
2017-09-19 12:59:16 -0500

    add decompress bench

--- bench/run.cpp
+++ bench/run.cpp
@@ -21,13 +21,35 @@ static void BM_compress(benchmark::State& state, const char * data, size_t lengt
     }
 }
 
+static void BM_decompress(benchmark::State& state, const char * data, size_t length) // NOLINT google-runtime-references
+{
+
+    if (state.thread_index == 0)
+    {
+
+    }
+    while (state.KeepRunning())
+    {
+        std::string value = gzip::decompress(data, length);
+        benchmark::DoNotOptimize(value.data());
+        benchmark::ClobberMemory();
+    }
+    if (state.thread_index == 0)
+    {
+        // Teardown code here.
+    }
+}
+
 int main(int argc, char* argv[])
 {
     std::ifstream t("14-4685-6265.mvt");
-    std::string str((std::istreambuf_iterator<char>(t)),
+    std::string str_uncompressed((std::istreambuf_iterator<char>(t)),
                     std::istreambuf_iterator<char>());
 
-    benchmark::RegisterBenchmark("BM_compress", BM_compress, str.data(), str.size())->Threads(2)->Threads(4)->Threads(8); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
+    std::string str_compressed = gzip::compress(str_uncompressed.data(), str_uncompressed.size());
+
+    benchmark::RegisterBenchmark("BM_compress", BM_compress, str_uncompressed.data(), str_uncompressed.size())->Threads(1)->Threads(2)->Threads(4)->Threads(8); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
+    benchmark::RegisterBenchmark("BM_decompress", BM_decompress, str_compressed.data(), str_compressed.size())->Threads(1)->Threads(2)->Threads(4)->Threads(8); // NOLINT clang-analyzer-cplusplus.NewDeleteLeaks
     benchmark::Initialize(&argc, argv);
     benchmark::RunSpecifiedBenchmarks();
 
