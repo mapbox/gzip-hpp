15205f2c4b954135a89ec1653ff7cd29673b9b07
Artem Pavlenko <artem@mapnik.org>
2018-10-03 12:18:36 +0100

    format

--- include/gzip/utils.hpp
+++ include/gzip/utils.hpp
@@ -8,15 +8,15 @@ namespace gzip {
 inline bool is_compressed(const char* data, std::size_t size) noexcept
 {
     return size > 2 &&
-        (
-            // zlib
-            (
-                static_cast<uint8_t>(data[0]) == 0x78 &&
-                (static_cast<uint8_t>(data[1]) == 0x9C ||
-                 static_cast<uint8_t>(data[1]) == 0x01 ||
-                 static_cast<uint8_t>(data[1]) == 0xDA ||
-                 static_cast<uint8_t>(data[1]) == 0x5E)) ||
-            // gzip
-            (static_cast<uint8_t>(data[0]) == 0x1F && static_cast<uint8_t>(data[1]) == 0x8B));
+           (
+               // zlib
+               (
+                   static_cast<uint8_t>(data[0]) == 0x78 &&
+                   (static_cast<uint8_t>(data[1]) == 0x9C ||
+                    static_cast<uint8_t>(data[1]) == 0x01 ||
+                    static_cast<uint8_t>(data[1]) == 0xDA ||
+                    static_cast<uint8_t>(data[1]) == 0x5E)) ||
+               // gzip
+               (static_cast<uint8_t>(data[0]) == 0x1F && static_cast<uint8_t>(data[1]) == 0x8B));
 }
 } // namespace gzip
